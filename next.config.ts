dc872795312e948c6f53794e89e328b28d3f9a9d
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-09-14 18:21:49 +0200

    feat(next-config): update configuration to support remote images from Alchemy API and add websocket as a server external package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.alchemyapi.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+  // This is needed for prod build due to alchemy-sdk using websocket
+  serverExternalPackages: ['websocket'],
 };
 
 export default nextConfig;
